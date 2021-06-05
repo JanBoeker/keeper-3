2a06ed4cc98a2dd2a53af0861aa9940364e0e523
Jan Böker <naj_b@gmx.de>
2021-06-05 21:51:11 +0200

    my solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notesCollection, manageCollection] = useState([]);
+
+  function addNote(newNote) {
+    manageCollection(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+    console.log(notesCollection);
+  }
+
+  function deleteNote(id) {
+    manageCollection((prevCollection) => {
+      return prevCollection.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+        onAdd={addNote}
+      />
+      {notesCollection.map((note, index) => {
+        return (
+          <Note 
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
